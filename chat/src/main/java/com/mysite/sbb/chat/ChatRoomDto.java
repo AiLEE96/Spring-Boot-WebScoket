24bc2cf2d5cc02abcc84ab2f7e28221debf8c00a
AiLEE96 <77310478+AiLEE96@users.noreply.github.com>
2023-02-13 17:00:04 +0900

    2023/02/13

---
+++ chat/src/main/java/com/mysite/sbb/chat/ChatRoomDto.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.chat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ChatRoomDto {
+    private String roomId;
+    private String roomName;
+
+    public static ChatRoomDto create(String name) {
+        ChatRoomDto room = new ChatRoomDto();
+        room.roomId = UUID.randomUUID().toString();
+        room.roomName = name;
+        return room;
+    }
+
+    //룸 아이디, 룸 네임.
+    //룸 아이디를 임의로 부여해서 방을 생성.
+    
+}

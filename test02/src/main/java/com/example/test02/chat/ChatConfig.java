438cf6cce3106edad85dbfc6ee6e33b403d843e9
AiLEE96 <77310478+AiLEE96@users.noreply.github.com>
2023-02-18 11:16:25 +0900

    2023/02/18

--- test02/src/main/java/com/example/test02/chat/ChatConfig.java
+++ test02/src/main/java/com/example/test02/chat/ChatConfig.java
@@ -15,11 +15,12 @@ import lombok.RequiredArgsConstructor;
 public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 
     private final ChatStompHandler chatStompHandler;
+    //JWT 토큰을 인증을 위한 핸들러
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat").setAllowedOriginPatterns("*").withSockJS();
-        //registry.addEndpoint("/ws/chat").withSockJS();
+        //웹 소켓 통신을 하기위한 엔드포인트 설정
     }
 
     @Override
@@ -28,10 +29,13 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/queue", "/topic");
 
         registry.setApplicationDestinationPrefixes("/app");
+        //메시지를 주고 받는 목적지 설정과 통신시에 /app 아래로 통신이 이뤄진다.
     }
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(chatStompHandler);
+        //토큰 검증을 위한 인터셉터 설정으로
+        //위에서 선언한 chatStompHandler를 인터셉터로 지정했다.
     }
 }
